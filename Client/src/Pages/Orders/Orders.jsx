bc817d5bc4b98d9a528608c71e8b8f78bc8ebad7
Xico <francisco100eg@gmail.com>
2023-03-06 08:52:34 -0300

    Added myGigs page and Orders page

--- Client/src/Pages/Orders/Orders.jsx
+++ Client/src/Pages/Orders/Orders.jsx
@@ -1,8 +1,129 @@
 import React from 'react'
 import './Orders.scss'
+import { Link } from 'react-router-dom'
+import './Orders.scss'
+import messageIcon from '../../assets/message.png'
+
 function Orders() {
+  const currentUser = {
+    img: "https://st3.depositphotos.com/6672868/13701/v/450/depositphotos_137014128-stock-illustration-user-profile-icon.jpg",
+    id: 1,
+    username: "FranciscoCosta",
+    isSeller: true,
+  };
+
   return (
-    <div>Orders</div>
+    <div className='Orders'>
+      <div className="Orders__container">
+        <div className="Orders__title">
+        <h1>{currentUser.isSeller ? "Gigs" : "Orders"}</h1>
+          {currentUser.isSeller && (
+            <Link to="/messages">
+              <button>Add New Gig</button>
+            </Link>
+          )}
+        </div>
+        <table>
+          <tr>
+            <th>Image</th>
+            <th>Title</th>
+            <th>Price</th>
+            <th>{currentUser.isSeller ? "Buyer" : "Selller"}</th>
+            <th>Contact</th>
+          </tr>
+          <tr>
+            <td>
+              <img
+                className="image"
+                src="https://images.pexels.com/photos/270408/pexels-photo-270408.jpeg?auto=compress&cs=tinysrgb&w=1600"
+                alt=""
+              />
+            </td>
+            <td>Stunning concept art</td>
+            <td>59.<sup>99</sup></td>
+            <td>13</td>
+            <td>
+              <img className="delete" src={messageIcon} alt="" />
+            </td>
+          </tr>
+          <tr>
+            <td>
+              <img
+                className="image"
+                src="https://images.pexels.com/photos/270408/pexels-photo-270408.jpeg?auto=compress&cs=tinysrgb&w=1600"
+                alt=""
+              />
+            </td>
+            <td>Ai generated concept art</td>
+            <td>120.<sup>99</sup></td>
+            <td>41</td>
+            <td>
+              <img className="delete" src={messageIcon} alt="" />
+            </td>
+          </tr>
+          <tr>
+            <td>
+              <img
+                className="image"
+                src="https://images.pexels.com/photos/270408/pexels-photo-270408.jpeg?auto=compress&cs=tinysrgb&w=1600"
+                alt=""
+              />
+            </td>
+            <td>High quality digital character</td>
+            <td>79.<sup>99</sup></td>
+            <td>55</td>
+            <td>
+              <img className="delete" src={messageIcon} alt="" />
+            </td>
+          </tr>
+          <tr>
+            <td>
+              <img
+                className="image"
+                src="https://images.pexels.com/photos/270408/pexels-photo-270408.jpeg?auto=compress&cs=tinysrgb&w=1600"
+                alt=""
+              />
+            </td>
+            <td>Illustration hyper realistic painting</td>
+            <td>119.<sup>99</sup></td>
+            <td>29</td>
+            <td>
+              <img className="delete" src={messageIcon} alt="" />
+            </td>
+          </tr>
+          <tr>
+            <td>
+              <img
+                className="image"
+                src="https://images.pexels.com/photos/270408/pexels-photo-270408.jpeg?auto=compress&cs=tinysrgb&w=1600"
+                alt=""
+              />
+            </td>
+            <td>Original ai generated digital art</td>
+            <td>59.<sup>99</sup></td>
+            <td>34</td>
+            <td>
+              <img className="delete" src={messageIcon} alt="" />
+            </td>
+          </tr>
+          <tr>
+            <td>
+              <img
+                className="image"
+                src="https://images.pexels.com/photos/270408/pexels-photo-270408.jpeg?auto=compress&cs=tinysrgb&w=1600"
+                alt=""
+              />
+            </td>
+            <td>Text based ai generated art</td>
+            <td>110.<sup>99</sup></td>
+            <td>16</td>
+            <td>
+              <img className="delete" src={messageIcon} alt="" />
+            </td>
+          </tr>
+        </table>
+      </div>
+    </div>
   )
 }
 
