22ddfe07032fdaa974194f3a49c0dceb0097cec5
Xico <francisco100eg@gmail.com>
2023-03-06 09:46:43 -0300

    Added messages page

--- Client/src/Pages/Messages/Messages.jsx
+++ Client/src/Pages/Messages/Messages.jsx
@@ -1,10 +1,85 @@
-import React from 'react'
-import './Messages.scss'
+import React from "react";
+import { Link } from "react-router-dom";
+import "./Messages.scss";
 
-function Messages() {
+const Messages = () => {
+  const currentUser = {
+    img: "https://st3.depositphotos.com/6672868/13701/v/450/depositphotos_137014128-stock-illustration-user-profile-icon.jpg",
+    id: 1,
+    username: "FranciscoCosta",
+    isSeller: true,
+  };
+
+  const message = `Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident
+  maxime cum corporis esse aspernatur laborum dolorum? Animi
+  molestias aliquam, cum nesciunt, aut, ut quam vitae saepe repellat
+  nobis praesentium placeat.`;
+
   return (
-    <div>Messages</div>
-  )
-}
+    <div className="Messages">
+      <div className="Messages__container">
+        <div className="Messages__title">
+          <h1>Messages</h1>
+        </div>
+        <table>
+          <tr>
+            <th>{currentUser.isSeller ? "Buyer" : "Seller"}</th>
+            <th>Last Message</th>
+            <th>Date</th>
+            <th>Action</th>
+          </tr>
+          <tr className="active">
+            <td>Charley Sharp</td>
+            <td>
+              <Link to="/message/123" className="link">
+                {message.substring(0, 100)}...
+              </Link>
+            </td>
+            <td>1 hour ago</td>
+            <td>
+              <button>Mark as Read</button>
+            </td>
+          </tr>
+          <tr className="active">
+            <td>John Doe</td>
 
-export default Messages
+            <td>
+              <Link to="/message/123" className="link">
+                {message.substring(0, 100)}...
+              </Link>
+            </td>
+            <td>2 hours ago</td>
+            <td>
+              <button>Mark as Read</button>
+            </td>
+          </tr>
+          <tr>
+            <td>Elinor Good</td>
+            <td>
+              <Link to="/message/123" className="link">
+                {message.substring(0, 100)}...
+              </Link>
+            </td>
+            <td>1 day ago</td>
+          </tr>
+          <tr>
+            <td>Garner David </td>
+            <td>
+              <Link to="/message/123" className="link">
+                {message.substring(0, 100)}...
+              </Link>
+            </td>
+            <td>2 days ago</td>
+          </tr>
+          <tr>
+            <td>Troy Oliver</td>
+            <td>{message.substring(0, 100)}</td>
+            <td>1 week ago</td>
+          </tr>
+        </table>
+      </div>
+    </div>
+  );
+};
+
+export default Messages;
