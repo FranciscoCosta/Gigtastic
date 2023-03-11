a0b342d84c6377013c2e166cdad7c96dc0addfc1
Xico <francisco100eg@gmail.com>
2023-03-11 20:11:09 -0300

    Feat: Added reviewModel

---
+++ Api/models/reviewModel.js
@@ -0,0 +1,31 @@
+import mongoose from "mongoose";
+const { Schema } = mongoose;
+
+const reviewSchema = new Schema(
+  {
+    gigId: {
+      type: String,
+      required: true,
+    },
+    userId: {
+      type: String,
+      required: true,
+    },
+    stars: {
+      type: Number,
+      required: true,
+      enum: [1, 2, 3, 4, 5],
+    },
+    desc: {
+      type: String,
+      required: true,
+      trim: true,
+      minlength: 3,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+export default mongoose.model("Review", reviewSchema);
