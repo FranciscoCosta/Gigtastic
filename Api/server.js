2d6037216ebb249d98a9a8549250bcdd7064fd32
Xico <francisco100eg@gmail.com>
2023-03-11 18:57:22 -0300

    Feat: Added dotenv Dependencie and MongoDB conection

--- Api/server.js
+++ Api/server.js
@@ -1,7 +1,20 @@
-const express = require("express");
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
 
 const app = express();
 
-app.listen(1234,()=>{
-    console.log("Backend server runing");
-})
+dotenv.config();
+
+mongoose.set("strictQuery", true);
+
+try {
+  await mongoose.connect(process.env.MONGO_URL);
+  console.log("Database connected");
+} catch (err) {
+  console.log(err);
+}
+
+app.listen(1234, () => {
+  console.log("Backend server runing");
+});
