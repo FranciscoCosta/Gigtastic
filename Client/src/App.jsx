abd327d6cd6fa7161c25c0aa47d1e6b04427d31b
Xico <francisco100eg@gmail.com>
2023-03-13 08:34:43 -0300

    Feat: Login page and logic to login

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -1,73 +1,84 @@
-import React from 'react'
-import { Gig, Gigs, Home, Orders, MyGigs, Add, Message, Messages, Register } from './Pages/index'
-import { Navbar, Footer } from './Components/index'
-import './App.scss'
+import React from "react";
 import {
-  createBrowserRouter, RouterProvider, Outlet
-} from "react-router-dom"
+  Gig,
+  Gigs,
+  Home,
+  Orders,
+  MyGigs,
+  Login,
+  Add,
+  Message,
+  Messages,
+  Register,
+} from "./Pages/index";
+import { Navbar, Footer } from "./Components/index";
+import "./App.scss";
+import { createBrowserRouter, RouterProvider, Outlet } from "react-router-dom";
 
 function App() {
-
-  const Layout =()=>{
-    return(
+  const Layout = () => {
+    return (
       <>
-      <Navbar/>
-      <Outlet/>
-      <Footer/>
+        <Navbar />
+        <Outlet />
+        <Footer />
       </>
-    )
-  }
+    );
+  };
 
   const router = createBrowserRouter([
     {
-      path: '/',
-      element: <Layout/>,
+      path: "/",
+      element: <Layout />,
       children: [
         {
-          path: '/',
-          element: <Home/>
+          path: "/",
+          element: <Home />,
         },
         {
-          path: '/gigs',
-          element: <Gigs/>
+          path: "/gigs",
+          element: <Gigs />,
         },
         {
-          path: '/register',
-          element: <Register/>
+          path: "/register",
+          element: <Register />,
         },
         {
-          path: '/gig/:id',
-          element: <Gig/>
+          path: "/gig/:id",
+          element: <Gig />,
         },
         {
-          path: '/orders',
-          element: <Orders/>
+          path: "/orders",
+          element: <Orders />,
         },
         {
-          path: '/mygigs',
-          element: <MyGigs/>
+          path: "/mygigs",
+          element: <MyGigs />,
         },
         {
-          path: '/add',
-          element: <Add/>
+          path: "/add",
+          element: <Add />,
         },
         {
-          path: '/message/:id',
-          element: <Message/>
+          path: "/message/:id",
+          element: <Message />,
         },
         {
-          path: '/messages',
-          element: <Messages/>
+          path: "/messages",
+          element: <Messages />,
         },
-        ]
-    }
-  ]
-  )
+        {
+          path: "/login",
+          element: <Login />,
+        },
+      ],
+    },
+  ]);
   return (
-    <div className='App'>
+    <div className="App">
       <RouterProvider router={router} />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
--- Client/src/Pages/Login/Login.jsx
+++ Client/src/Pages/Login/Login.jsx
@@ -1,10 +1,57 @@
-import React from 'react'
-import './Login.scss'
+import React, { useState } from "react";
+import "./Login.scss";
+import { useNavigate } from "react-router-dom";
+import axios from "axios";
 
 function Login() {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState(null);
+
+  const navigate = useNavigate();
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const res = await axios.post(
+        "http://localhost:8080/api/v1/login",
+        {
+          username,
+          password,
+        },
+        { withCredentials: true }
+      );
+      localStorage.setItem("currentUser", JSON.stringify(res.data));
+      navigate("/");
+    } catch (err) {
+      setError(err.response.data);
+    }
+  };
+
   return (
-    <div>Login</div>
-  )
+    <div className="Login">
+      <form onSubmit={handleSubmit}>
+        <h1>Sign in</h1>
+        <label htmlFor="">Username</label>
+        <input
+          name="username"
+          type="text"
+          placeholder="Username"
+          onChange={(e) => setUsername(e.target.value)}
+        />
+
+        <label htmlFor="">Password</label>
+        <input
+          name="password"
+          type="password"
+          placeholder="Password"
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <button type="submit">Login</button>
+        {error && error}
+      </form>
+    </div>
+  );
 }
 
-export default Login
+export default Login;
