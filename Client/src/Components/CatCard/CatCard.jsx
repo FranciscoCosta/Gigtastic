177640ae01a6f16d9be28edd086bd139db6be9b0
Xico <francisco100eg@gmail.com>
2023-03-15 18:02:10 -0300

    Fix: Fixed carrousel categories

--- Client/src/Components/CatCard/CatCard.jsx
+++ Client/src/Components/CatCard/CatCard.jsx
@@ -1,16 +1,30 @@
-import React from 'react'
+import React, {useContext} from 'react'
 import './CatCard.scss'
-import { Link } from 'react-router-dom'
+import { Context } from '../../Context/context';
+import { useNavigate } from 'react-router-dom';
+
 
 function CatCard({item}) {
+
+  const navigate = useNavigate();
+  const { setCategory, setSearch, setuserFilter } = useContext(Context);
+
+  const handleClick = (category) => {
+    setCategory(category);
+    setSearch("");
+    setuserFilter([]);
+    navigate("/gigs");
+    
+
+  }
   return (
-        <Link to="/gigs">
-        <div className='CatCard'>
+        <div 
+        onClick={()=>handleClick(item.category)}
+        className='CatCard'>
             <img src={item.img} alt="" />
             <p className='descripton'>{item.desc} </p>
             <h2 className='title'>{item.title}</h2>
             </div>
-            </Link>
   )
 }
 
